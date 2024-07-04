bbd7b4c1f97af73eccc6aab46aa920c2ff3f1d0a
gtraiano <gtraiano@gmail.com>
2024-07-04 21:38:11 +0300

    refactored event handlers

--- script.js
+++ script.js
@@ -11,28 +11,34 @@ const input = document.querySelector("form input#number");
 const form = document.querySelector("form");
 const submit = document.querySelector("form input[type='submit']");
 
-input.addEventListener("input", (e) => {
-  // perform validation on input
+// input event handler
+const onInput = e => {
+  // disable submit button on empty input
+  if(!e.currentTarget.value.trim().length) return submit.setAttribute("disabled", "");
+  
   const isValid = validateInput(e.currentTarget.value, validator);
   // mark field as invalid and disable submit button depending on input validation
   if (!isValid) {
-    input.setCustomValidity("Value must be greater than 10");
+    e.currentTarget.setCustomValidity("Value must be greater than 10");
     submit.setAttribute("disabled", "");
-  } else {
-    input.setCustomValidity("");
+  }
+  else {
+    e.currentTarget.setCustomValidity("");
     submit.removeAttribute("disabled");
   }
-});
+};
 
-// form submit cleanup
-form.addEventListener("submit", (e) => {
+// form submit event handler
+const onSubmit = e => {
   e.preventDefault();
   alert(`You submitted ${input.value}`);
   input.value = "";
-});
+}
+
+input.addEventListener("input", onInput);
+form.addEventListener("submit", onSubmit);
 
 // disable submit button on initial load if input value is empty
 document.addEventListener("DOMContentLoaded", () => {
   !input.value.trim().length && submit.setAttribute("disabled", "");
 });
-
